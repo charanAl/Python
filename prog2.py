10a7c5eb78cd9d25b6b4f41389b7665f73b272db
A.Charan <159403982+charanAl@users.noreply.github.com>
2026-01-28 09:51:46 +0530

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,17 @@
+class College:
+    College_name = "Aurora's Technological Institutef"
+    def __init__(self, name, age, marks):
+        self.name = name
+        self.age = age
+        self.marks = marks
+
+s1 = College("Arthi", 20, 85)
+s2 = College('Aravind', 22, 90)
+s3 = College('Charan', 20, 98)
+
+print(s1.name)
+print(s3.name)
+print(s1.College_name)
+print(s2.name)
+print(s2.College_name)
+        
