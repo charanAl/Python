fd2a65f8202e0b9b849069365fd939f2c403f392
A.Charan <159403982+charanAl@users.noreply.github.com>
2026-02-01 21:31:01 +0530

    Add files via upload

---
+++ prog5.py
@@ -0,0 +1,11 @@
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+obj1 = person("Arthi", 20)
+obj2 = person("Charan", 21)
+
+print(obj1.name)
+print(obj2.name)
+print(obj1.age)
+print(obj2.age)
