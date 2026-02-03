207795759c2014bcd79228281317817bc1602d46
A.Charan <159403982+charanAl@users.noreply.github.com>
2026-02-03 21:11:36 +0530

    Add files via upload

---
+++ prog6.py
@@ -0,0 +1,12 @@
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def run(self):
+        print('person can run!')
+
+obj2 = person("Charan", 21)
+
+obj2.run()
+    
