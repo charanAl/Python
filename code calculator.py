1727555fca880a54794b7c559551ac3c70d4ff26
Alle Charan <159403982+charanAl@users.noreply.github.com>
2026-02-22 09:59:05 +0530

    Add files via upload

---
+++ code calculator.py
@@ -0,0 +1,52 @@
+# def main():
+#     x=int(input("What's x?"))
+#     print("x squared is :", square(x))
+
+# def square(n):
+#     return n**2
+# main()
+
+
+x = int(input("What's X?"))
+y = int(input("What's Y?"))
+
+# if x>y:
+#     print("x is greater than y")
+# elif x<y:
+#     print("x is less than y")
+# else:
+#     print("X is equal to y")
+    
+if x!=y:
+    print("x is not equalt to y")
+else:
+    print('x is equal to y')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# x = int(input("What's X?"))
+# y = int(input("What's Y?"))
+
+# z = x+y
+# print(z)
+
+# a = float(input("what is the a value"))
+# b = float(input("what is b value"))
+# c =round( a+b)
+# print(f"{c:,}")
