1901c79ce7c791ffd05ba1cf2be9cc89594cb771
Alle Charan <159403982+charanAl@users.noreply.github.com>
2026-02-22 12:22:24 +0530

    Add files via upload

---
+++ tast1.py
@@ -0,0 +1,15 @@
+
+user1=int(input("Enter a number:"))
+
+user2=int(input("Enter a number:"))
+sum = user1 + user2
+print('Their sum:',sum)
+
+
+
+
+
+
+# Enter 2 numbers from user
+# Print their sum
+# Print their datatype
