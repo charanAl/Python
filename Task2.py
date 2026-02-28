3f7a7f624d27995a854ee42ed78082baaf54d253
Alle Charan <159403982+charanAl@users.noreply.github.com>
2026-02-28 09:30:56 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,8 @@
+# Password Checker
+
+correct_password =  'python@123'
+user_password = input("Enter your password:")
+if user_password == correct_password:
+    print("Access granted")
+else:
+    print("Access denied")
