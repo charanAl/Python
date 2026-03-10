c062c7c16e9c98c9a838ccfc374d4bf0ef56f140
Alle Charan <159403982+charanAl@users.noreply.github.com>
2026-03-10 10:05:57 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,14 @@
+full_name = "Charan"
+age = 23
+gpa = 8.1
+is_student = False
+
+print(f"Hello {full_name}")
+print(f"age:{age}")
+print(f"gpa{gpa}")
+print(f"isstudent{is_student}")
+
+if is_student:
+    print("Your a student")
+else:
+    print("Your not a student")
